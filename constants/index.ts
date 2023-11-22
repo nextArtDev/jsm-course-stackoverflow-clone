a9ab64ffe79812b2e41520bfd3c365deed94cdb7
Saeed Mehmanparast <nextartisticdevelopments@gmail.com>
2023-11-22 21:27:53 +0330

    navbar

--- constants/index.ts
+++ constants/index.ts
@@ -1,45 +1,45 @@
 import { SidebarLink } from '@/types'
 
 export const themes = [
-  { value: 'light', label: 'Light', icon: '/assets/icons/sun.svg' },
-  { value: 'dark', label: 'Dark', icon: '/assets/icons/moon.svg' },
-  { value: 'system', label: 'System', icon: '/assets/icons/computer.svg' },
+  { value: 'light', label: 'روز', icon: '/assets/icons/sun.svg' },
+  { value: 'dark', label: 'شب', icon: '/assets/icons/moon.svg' },
+  { value: 'system', label: 'سیستم', icon: '/assets/icons/computer.svg' },
 ]
 export const sidebarLinks: SidebarLink[] = [
   {
     imgURL: '/assets/icons/home.svg',
     route: '/',
-    label: 'Home',
+    label: 'خانه',
   },
   {
     imgURL: '/assets/icons/users.svg',
     route: '/community',
-    label: 'Community',
+    label: 'کامیونیتی',
   },
   {
     imgURL: '/assets/icons/star.svg',
     route: '/collection',
-    label: 'Collections',
+    label: 'کلکسیون',
   },
   {
     imgURL: '/assets/icons/suitcase.svg',
     route: '/jobs',
-    label: 'Find Jobs',
+    label: 'شغلها',
   },
   {
     imgURL: '/assets/icons/tag.svg',
     route: '/tags',
-    label: 'Tags',
+    label: 'تگها',
   },
   {
     imgURL: '/assets/icons/user.svg',
     route: '/profile',
-    label: 'Profile',
+    label: 'پروفایل',
   },
   {
     imgURL: '/assets/icons/question.svg',
     route: '/ask-question',
-    label: 'Ask a question',
+    label: 'سوال پرسیدن',
   },
 ]
 export const BADGE_CRITERIA = {
--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -31,7 +31,6 @@ export function ThemeProvider({ children }: { children: ReactNode }) {
     }
   }
 
-  //
   useEffect(() => {
     handleThemeChange()
   }, [mode])
